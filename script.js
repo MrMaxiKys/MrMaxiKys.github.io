b130e682dae9d4304991109bf71e5839e1bb458c
MrMaxiKys <cris_tian@wp.pl>
2021-05-06 10:00:33 +0200

    Initial commit

--- script.js
+++ script.js
@@ -7,24 +7,3 @@ var slideUp = {
 };
 
 ScrollReveal().reveal('.slide', slideUp);
-
-//Button function
-
-//Get the button
-var mybutton = document.getElementById("myBtn");
-//When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-}
-
-//When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}

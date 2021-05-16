cd9bf9331507b97119fc8eac4854aa5ee16f98cb
MaxiKys <57059627+MrMaxiKys@users.noreply.github.com>
2021-05-16 12:50:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -50,28 +50,24 @@ function topFunction() {
 
 // Modal function
 
-// Create references to the modal...
 var modal = document.getElementById('myModal');
-// To all images -- note I'm using a class!
 var images = document.getElementsByClassName('myImages');
-// The image in the modal
 var modalImg = document.getElementById("img01");
-// And the caption in the modal
 var captionText = document.getElementById("caption");
 
 // Go through all of the images with our custom class
 for (var i = 0; i < images.length; i++) {
-  var img = images[i];
+    var img = images[i];
   // And attach our click listener for this image.
-  img.onclick = function(evt) {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+    img.onclick = function(evt) {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
   }
 }
 
 var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
